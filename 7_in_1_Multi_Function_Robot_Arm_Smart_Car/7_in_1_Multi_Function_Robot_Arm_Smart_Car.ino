694338c99bf884019d381c20d5b20cc0a449ff6e
Павел <apaex@yandex.ru>
2025-01-07 13:12:56 +0300

    -

--- 7_in_1_Multi_Function_Robot_Arm_Smart_Car/7_in_1_Multi_Function_Robot_Arm_Smart_Car.ino
+++ 7_in_1_Multi_Function_Robot_Arm_Smart_Car/7_in_1_Multi_Function_Robot_Arm_Smart_Car.ino
@@ -218,22 +218,8 @@ void commandInterpretator(char cmd)
 
 void setFromStickPositions(const GamepadData &package)
 { 
-/* 
-  SerialPrintf(
-      "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
-      "misc: 0x%02x\n",
-      package.index,        // Controller Index
-      package.dpad,         // D-pad
-      package.buttons,      // bitmask of pressed buttons
-      package.axisX,        // (-511 - 512) left X Axis
-      package.axisY,        // (-511 - 512) left Y axis
-      package.axisRX,       // (-511 - 512) right X axis
-      package.axisRY,       // (-511 - 512) right Y axis
-      package.brake,        // (0 - 1023): brake button
-      package.throttle,     // (0 - 1023): throttle (AKA gas) button
-      package.miscButtons  // bitmask of pressed "misc" buttons
-  ); 
-*/  
+  //DebugWrite(package);
+  
   int vx = package.axisY;
   int rz = package.axisRX;    
 
--- 7_in_1_Multi_Function_Robot_Arm_Smart_Car/gamepad.h
+++ 7_in_1_Multi_Function_Robot_Arm_Smart_Car/gamepad.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "serialPrintf.h"
+
 struct GamepadData
 {
   uint8_t index;
@@ -13,3 +15,21 @@ struct GamepadData
   uint16_t throttle;
   uint8_t miscButtons;
 };
+
+
+void DebugWrite(const GamepadData& package)
+{
+  SerialPrintf(
+      "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, misc: 0x%02x\n",
+      package.index,        // Controller Index
+      package.dpad,         // D-pad
+      package.buttons,      // bitmask of pressed buttons
+      package.axisX,        // (-511 - 512) left X Axis
+      package.axisY,        // (-511 - 512) left Y axis
+      package.axisRX,       // (-511 - 512) right X axis
+      package.axisRY,       // (-511 - 512) right Y axis
+      package.brake,        // (0 - 1023): brake button
+      package.throttle,     // (0 - 1023): throttle (AKA gas) button
+      package.miscButtons  // bitmask of pressed "misc" buttons
+  ); 
+}
