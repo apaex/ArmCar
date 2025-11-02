f4fad9ae8763dc16fe0585272afa963d5147541d
Павел <apaex@yandex.ru>
2025-11-02 22:13:35 +0300

    9600

--- 7_in_1_Multi_Function_Robot_Arm_Smart_Car/settings.h
+++ 7_in_1_Multi_Function_Robot_Arm_Smart_Car/settings.h
@@ -65,7 +65,7 @@
 #define SERVO_POLLING_PERIOD (48 >> SERVO_SCALE_FACTOR)     // по (48 >> 3) = 6 мс
 #define SERVO_DEFAULT_VELOCITY 64
 
-#define BAUD_RATE 115200
+#define BAUD_RATE 9600
 #define SERIAL_TIMEOUT 1000
 
 #define DISPLAY_NCOL        20           // размер дисплея: ширина
